62425660a5c24035be8947f77654f1e520f43ad2
CyberFlareTesting <88449302+CyberFlareTesting@users.noreply.github.com>
2021-08-04 13:53:27 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -2,10 +2,20 @@ const express = require("express")
 const app = express()
 const port = process.env.PORT || 3000
 
+app.use(express.static(__dirname + "/css"));
+//app.use(express.static(__dirname + "/js"))
+//app.use(express.static(__dirname + "/other"))
+
+// pages
 app.get('/', function (req, res) {
-    res.send('<h1>Hello World!</h1>')
+    res.redirect('/Convert')
 })
 
+app.get('/Convert',function(req, res) {
+    res.sendFile(__dirname + '/html/index.html')
+})
+
+// server
 app.listen(port, function () {
-    console.log("Server is running on "+ port +" port");
+    console.log("Server is running on port "+ port);
 });
